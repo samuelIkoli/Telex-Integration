1a0fdbdea017a94d69d8bee3b7aa3c3ef9c6673f
samuelIkoli <ayibanimiikoli@gmail.com>
2025-02-18 00:09:27 +0100

    edit settings

--- index.js
+++ index.js
@@ -25,20 +25,7 @@ app.get("/telex-webhook", (req, res) => {
                 "integration_category":"Monitoring & Logging",
                 "key_features":["Real time notifications"],
                 "author":"Samuel Ikoli",
-                "settings":[{
-                    "label":"Time interval","type":"dropdown",
-                    "description": "Maximum number of recent messages to use as context (default: 50)",
-                    "required":true,
-                    "default":"Immediate",
-                    "options":["Immediate","Every 5 minutes","Every 10 minutes","Every 30 minutes"]
-                },
-                    {
-                    "label": "messageLimit",
-                    "type": "number",
-                    "description": "Maximum number of recent messages to use as context (default: 50)",
-                    "default": 50,
-                    "required": false
-                    }],
+                "settings":[{"label":"customProfaneWords","type":"multi-select","description":"Select custom profane words to track.","required":true,"default":"fuck,shit,ass,bastard,bitch,dick,cock,pussy,damn,fucking,motherfucker,asshole,prick,dumbass,fucktard,shithead"},{"label":"caseSensitivity","type":"checkbox","description":"Enable case-sensitive profanity detection. If checked, 'Fuck' and 'fuck' will be treated differently.","default":false},{"label":"maskingStyle","type":"dropdown","options":["asterisks","dashes","partial"],"description":"Choose how to mask detected profane words.","default":"partial","required":true},{"label":"actionOnDetection","type":"dropdown","options":["flag","block","replace"],"description":"Decide how to handle detected messages.","default":"flag","required":true},{"label":"notificationOnDetection","type":"dropdown","options":["Yes","No"],"description":"Notify admins when a profane message is detected.","default":"No","required":true},{"label":"WebhookUrl","type":"text","description":"Specify the webhook url of the channel to notify admin","default":"https://my-webhook-url.com","required":true},{"label":"maxProfanityCount","type":"number","description":"Set the maximum allowed profane words per message.","default":3,"required":true}],
                 "target_url":"https://hooks.slack.com/services/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"}})
 })
 
