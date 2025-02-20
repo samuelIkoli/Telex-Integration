a2fd333ed9014db28ce150caa7b2481f8203eae5
samuelIkoli <ayibanimiikoli@gmail.com>
2025-02-20 18:17:01 +0100

    broooo

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const https = require("https");
-const integration = require("./integration");
 
 const app = express();
 
@@ -14,6 +13,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/integration.json", (req, res) => {
+  const baseUrl = `${req.protocol}://${req.get("host")}`;
+
+  const integration = {
+    data: {
+      date: {
+        created_at: "2025-02-20",
+        updated_at: "2025-02-20",
+      },
+      descriptions: {
+        app_description:
+          "SSL-Sentinel periodically check the SSL certificates of provided websites, and reports the SSL expiration date for the website",
+        app_logo:
+          "https://i.pinimg.com/736x/9a/6c/12/9a6c121be8cc5b037526f9922b956db1.jpg",
+        app_name: "SSL-Sentinel",
+        app_url: `${baseUrl}`,
+        background_color: "#50C878",
+      },
+      integration_category: "Monitoring & Logging",
+      integration_type: "interval",
+      is_active: true,
+      key_features: [
+        "Periodically checks SSL/TLS certificates for expiration.",
+        "Sends periodics updates.",
+        "Retrieves issuer, validity and encryption details.",
+      ],
+      settings: [
+        { label: "site-1", type: "text", required: true, default: "" },
+        { label: "site-2", type: "text", required: true, default: "" },
+        {
+          label: "interval",
+          type: "text",
+          required: true,
+          default: "*/5 * * * *",
+        },
+      ],
+      tick_url: `${baseUrl}/tick`,
+      target_url: "",
+    },
+  };
   return res.json(integration);
 });
 
